93804aa61885b4a2748ebf4aeaf087d07dd11a96
safalafas <chau0317@algonquinlive.com>
2026-05-20 18:01:39 -0400

    build: upgrade backend and frontend dependencies to up-to-date stable versions

--- config/app.php
+++ config/app.php
@@ -181,7 +181,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -208,9 +207,6 @@ return [
     |
     */
 
-    'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
-        'Image' => Intervention\Image\Facades\Image::class
-    ])->toArray(),
+    'aliases' => Facade::defaultAliases()->toArray(),
 
 ];
